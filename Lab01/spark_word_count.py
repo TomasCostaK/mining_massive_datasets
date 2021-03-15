1f8ae4052b3c5f70ec521634040101e7cbe4957d
Tom1k <tomascosta@ua.pt>
2021-03-15 23:26:59 +0000

    Started bigram generation, need to finish  it

--- Lab01/spark_word_count.py
+++ Lab01/spark_word_count.py
@@ -10,12 +10,17 @@ if __name__ == "__main__":
     sc = SparkContext(appName="SparkIntro_WordCount")
 
     textfile = sc.textFile(sys.argv[1])
-    occurences = textfile.flatMap(lambda line: re.split(r'[^\w]+', line.lower())) \
+    bigrams = textfile.flatMap(lambda line: re.split(r'[^\w]+', line.lower())) \
                             .filter(lambda w: len(w)>3) \
-                            .map(lambda word : (word, 1)) \
+                            .map(lambda tup: (tuple(x) for x in zip(tup[1],tup[1:]))) \
                             .reduceByKey(lambda a,b: a+b) \
                             .sortBy(lambda p: p[1], False)
+    """
+    bigrams.map(lambda word : (word, 1)) \
+                .reduceByKey(lambda a,b: a+b) \
+                .sortBy(lambda p: p[1], False)
+    """
 
     format_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    occurences.saveAsTextFile("{0}/{1}".format(sys.argv[2], format_time))
+    bigrams.saveAsTextFile("{0}/{1}".format(sys.argv[2], format_time))
     sc.stop()
