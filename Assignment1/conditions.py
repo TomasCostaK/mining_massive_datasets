a20eb2a058e7521807bf5040cf02aa83c0dea3f8
tom1k <tomascosta@ua.pt>
2021-03-24 15:12:32 +0000

    Count of each disease

--- Assignment1/conditions.py
+++ Assignment1/conditions.py
@@ -1,22 +1,32 @@
 from pyspark import SparkContext
 from datetime import datetime
-import operator
 import re
 import sys
+
+def preprocess_data(line):
+    # here we reorder the data that came in csv format
+    fields = re.split(',' ,line.lower())
+    #for i in range(len(fields) - 1):
+    single_case = fields[4] # code for disease
+    return single_case
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         exit(-1)
-    sc = SparkContext(appName="SocialMedia")
+        
+    sc = SparkContext(appName="Assignment1")
 
     textfile = sc.textFile(sys.argv[1])
-    occurences = textfile.flatMap(lambda line: re.split(r'[^\w]+', line.lower())) \
-                            .filter(lambda w: len(w)>3) \
-                            .map(lambda word : (word[0], word)) \
-                            .distinct() \
-                            .aggregateByKey(0,lambda v1,v2: v1+1,operator.add) \
+    bigrams = textfile.map(preprocess_data) \
+                            .map(lambda bigram : (bigram, 1)) \
+                            .reduceByKey(lambda a,b: a+b) \
                             .sortBy(lambda p: p[1], False)
+    """
+    bigrams.map(lambda word : (word, 1)) \
+                .reduceByKey(lambda a,b: a+b) \
+                .sortBy(lambda p: p[1], False)
+    """
 
     format_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    occurences.saveAsTextFile("{0}/{1}".format(sys.argv[2], format_time))
+    bigrams.saveAsTextFile("{0}/{1}".format(sys.argv[2], format_time))
     sc.stop()
