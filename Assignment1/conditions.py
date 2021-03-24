1d65ca3c21c83c9c13ebd75d50ca94b2db3acfef
tom1k <tomascosta@ua.pt>
2021-03-24 15:52:43 +0000

    First step of A-Priori

--- Assignment1/conditions.py
+++ Assignment1/conditions.py
@@ -1,7 +1,11 @@
 from pyspark import SparkContext
 from datetime import datetime
 import re
+import logging
 import sys
+
+# Este support deve ser 1000, mas
+SUPPORT_THRESHOLD = 6
 
 def preprocess_data(line):
     # here we reorder the data that came in csv format
@@ -13,19 +17,18 @@ def preprocess_data(line):
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         exit(-1)
-        
+    
+    # Starting context and opening file in command line
     sc = SparkContext(appName="Assignment1")
-
     textfile = sc.textFile(sys.argv[1])
+    
+    # Mapping patient as bucket to several diseases
     bigrams = textfile.map(preprocess_data) \
                             .reduceByKey(lambda a,b: a+","+b) \
+                            .filter(lambda a: len(re.split(',', a[1]))>SUPPORT_THRESHOLD) \
                             .sortBy(lambda p: p[1], False)
-    """
-    bigrams.map(lambda word : (word, 1)) \
-                .reduceByKey(lambda a,b: a+b) \
-                .sortBy(lambda p: p[1], False)
-    """
 
+    # Results formatting
     format_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     bigrams.saveAsTextFile("{0}/{1}".format(sys.argv[2], format_time))
     sc.stop()
