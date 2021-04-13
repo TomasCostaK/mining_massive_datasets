ef4d76568e1029d628439edb59035f85bc18615d
PedroOliveiraPT <pedromroliveirapt@gmail.com>
2021-04-13 18:07:59 +0100

    changed to add the association rules

--- Assignment1/conditions.py
+++ Assignment1/conditions.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 from datetime import datetime
 import re
 import logging
@@ -7,6 +8,8 @@ import collections
 
 # Este support deve ser 1000, mas
 SUPPORT_THRESHOLD = 6
+
+STD_LIFT_THRESHOLD = 0.2
 
 def build_bigram(basket, filtered_diseases):
     diseases = list(basket[1])
@@ -39,6 +42,25 @@ def build_trigram(basket, filtered_diseases, filtered_bigrams):
                 list_of_trigrams.append((d1 + "," + d2 + "," + d3, 1))
     return list_of_trigrams            
 
+def get_support(disease, confidence_rdd):
+    x = disease.rfind(",")
+    key = disease[:x]
+    print("am here", confidence_rdd.filter(lambda line: line[0] == key).values().collect())
+    return confidence_rdd.filter(lambda line: line[0] == key).values().collect()[0]
+
+def get_probability(disease, baskets_rdd):
+    x = disease.rfind(",")
+    key = disease[x+1:]
+    return baskets_rdd.filter(lambda line: key in set(line[1])).count()/baskets_rdd.count()
+
+def get_std_lift(disease, dlift, baskets_rdd, diseases_rdd):
+    p_group = get_support(disease, diseases_rdd)
+    p_d = get_probability(disease, baskets_rdd)
+    max_val = max(p_group + p_d - 1, 1/baskets_rdd.count())
+    numerator = dlift - max_val/(p_group*p_d)
+    denominator = 1/(p_group*p_d) - max_val/(p_group*p_d)
+    return numerator/denominator
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         exit(-1)
@@ -49,6 +71,7 @@ if __name__ == "__main__":
     
     # Starting context and opening file in command line
     sc = SparkContext(appName="Assignment1")
+    spark = SparkSession(sc)
     textfile = sc.textFile(sys.argv[2])
     
     # Mapping patient as bucket to several diseases
@@ -56,19 +79,38 @@ if __name__ == "__main__":
                         .map(lambda pair: (pair[2],pair[4])) \
                         .groupByKey()
 
-    filtered_diseases = baskets.flatMap(lambda line: [(code, 1) for code in line[1]]) \
-                                .reduceByKey(lambda a, b: a+b) \
-                                .filter(lambda line: line[1] > SUPPORT_THRESHOLD) \
+    diseases_support = baskets.flatMap(lambda line: [(code, 1) for code in line[1]]) \
+                                .reduceByKey(lambda a, b: a+b)
+    
+    filtered_diseases = diseases_support.filter(lambda line: line[1] > SUPPORT_THRESHOLD)\
                                 .map(lambda line: line[0]).collect()
 
-    bigrams = baskets.flatMap(lambda line: build_bigram(line, filtered_diseases))\
-                    .reduceByKey(lambda a, b: a+b) \
-                    .filter(lambda line: line[1] > SUPPORT_THRESHOLD)
+    bigrams_support = baskets.flatMap(lambda line: build_bigram(line, filtered_diseases))\
+                    .reduceByKey(lambda a, b: a+b)
+
+    bigrams = bigrams_support.filter(lambda line: line[1] > SUPPORT_THRESHOLD)
 
     # Results formatting
     if k == 2:
-        format_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        bigrams.saveAsTextFile("{0}/{1}".format(sys.argv[3], format_time))
+        association_rules = {key:[value] for key, value in bigrams_support.collect()}
+
+        for key in association_rules:
+            #Confidence level
+            association_rules[key][0] /= get_support(key, diseases_support)
+            
+            #Interest level
+            association_rules[key].append(association_rules[key][0] - get_probability(key, baskets))
+
+            #Lift
+            association_rules[key].append(association_rules[key][0]/get_probability(key, baskets))
+
+            #Std lift
+            association_rules[key].append(get_std_lift(key, association_rules[key][2], baskets, diseases_support))
+        
+        association_rules = association_rules.parallelize().filter(lambda line: line[4] > STD_LIFT_THRESHOLD)\
+                                        .sortBy(lambda line: line[4])\
+                                        .toDF(["Pair", "Confidence", "Interest", "Lift", "Standard Lift"])
+        association_rules.write.format("csv").save("{0}/{1}".format(sys.argv[3], "Association_Rules.csv"))
         sc.stop()
         exit(0)
         
