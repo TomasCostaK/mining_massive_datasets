bb72dc61674cb098536d369612239c43955f819f
PedroOliveiraPT <pedromroliveirapt@gmail.com>
2021-04-16 17:14:52 +0100

    documented main

--- Assignment1/conditions.py
+++ Assignment1/conditions.py
@@ -8,11 +8,13 @@ import sys
 import collections
 
 # Este support deve ser 1000, mas
-SUPPORT_THRESHOLD = 6
+SUPPORT_THRESHOLD = 1000
 
 STD_LIFT_THRESHOLD = 0.2
 
 def build_bigram(basket, filtered_diseases):
+    """ Function to build a bigram based on the basket and the diseases with more than the supported threshold
+    """
     diseases = list(basket[1])
     list_of_bigrams = []
     for i in range(len(diseases)):
@@ -27,6 +29,8 @@ def build_bigram(basket, filtered_diseases):
 
 
 def build_trigram(basket, filtered_diseases, filtered_bigrams):
+    """ Function to build a trigram based on the basket and the diseases with more than the supported threshold
+    """
     diseases = list(basket[1])
     list_of_trigrams = []
     for i in range(len(diseases)):
@@ -44,17 +48,20 @@ def build_trigram(basket, filtered_diseases, filtered_bigrams):
     return list_of_trigrams            
 
 def get_support(disease, confidence_df):
+    """Function to get the number of occurences the group I occurs in the dataset"""
     x = disease.rfind(",")
     key = disease[:x]
     return confidence_df.filter(confidence_df.Disease == key).collect()[0].Count
 
 def get_probability(disease, baskets_df):
+    """Function to get the probability of a disease j in the baskets"""
     from pyspark.sql.functions import array_contains
     x = disease.rfind(",")
     key = disease[x+1:]
     return baskets_df.filter(array_contains(baskets_df.DeseasesList, key)).count()/baskets_df.count()
 
 def get_std_lift(dsupp, dprob, dlift, basketscount):
+    """Function to get calculate the standard lift based on the support and probability calculated previously"""
     max_val = max(dsupp + dprob - 1, 1/basketscount)
     numerator = dlift - max_val/(dsupp*dprob)
     denominator = 1/(dsupp*dprob) - max_val/(dsupp*dprob)
@@ -79,24 +86,28 @@ if __name__ == "__main__":
                         .map(lambda pair: (pair[2],[pair[4]])) \
                         .reduceByKey(lambda a,b: a+b)
 
+    # Converting the baskets RDD to a Dataframe and saving it to RAM storage for easier access
     baskets_df = baskets.toDF(["User", "DeseasesList"]).cache().persist(StorageLevel.MEMORY_ONLY)
 
+    # Considering th baskets RDD, we filter to only get diseases with a support above the defined threshold, in this case 1000
     diseases_support = baskets.flatMap(lambda line: [(code, 1) for code in line[1]]) \
                                 .reduceByKey(lambda a, b: a+b).filter(lambda line: line[1] > SUPPORT_THRESHOLD)
 
+    # Again converting the RDD to a Dataframe and saving it to cache
     diseases_support_df = diseases_support.toDF(["Disease", "Count"]).cache().persist(StorageLevel.MEMORY_ONLY)
     
+    # Get the list of diseases from the Diseases RDD
     filtered_diseases = diseases_support.map(lambda line: line[0]).collect()
 
+    # Build the collection of Bigrams from the baskets, and filtering again to the support threshold
     bigrams_support = baskets.flatMap(lambda line: build_bigram(line, filtered_diseases))\
                     .reduceByKey(lambda a, b: a+b)\
                     .filter(lambda line: line[1] > SUPPORT_THRESHOLD)
 
-    # Results formatting
+    # If k=2 our job is done, we get the association rules and write them down to files
     if k == 2:
         association_rules = {key:[value] for key, value in bigrams_support.collect()}
         list_assoc_rules = []
-        print(len(association_rules))
 
         for key in association_rules:
             #Confidence level
@@ -115,7 +126,7 @@ if __name__ == "__main__":
         association_rules = association_rules.filter(lambda line: line[4] > STD_LIFT_THRESHOLD)\
                                         .sortBy(lambda line: line[4])
 
-        bigrams_support_top10 = bigrams_support.sortBy(lambda line: line[1]).take(10)
+        bigrams_support_top10 = bigrams_support.sortBy(lambda line: line[1], False).take(10)
 
         with open("{0}/Top 10 Bigrams.csv".format(sys.argv[3]), "w") as filewrite:
             filewrite.write("\n".join([b[0] for b in bigrams_support_top10]))
@@ -124,7 +135,10 @@ if __name__ == "__main__":
         association_rules.saveAsTextFile("{0}/Association Rules {1}".format(sys.argv[3], format_time))
         sc.stop()
         exit(0)
-        
+   
+    # Following the same logic as before, we get the filtered bigrams and save them in a list, build the trigrams out of those
+    # and create check the association rules before writing
+    
     filtered_bigrams = bigrams_support.map(lambda line: line[0]).collect()
 
     trigrams_support = baskets.flatMap(lambda line: build_trigram(line, filtered_diseases, filtered_bigrams))\
@@ -135,7 +149,6 @@ if __name__ == "__main__":
 
     association_rules = {key:[value] for key, value in trigrams_support.collect()}
     list_assoc_rules = []
-    print(len(association_rules))
 
     for key in association_rules:
         #Confidence level
@@ -149,15 +162,12 @@ if __name__ == "__main__":
         #Std lift
         association_rules[key].append(get_std_lift(supp, prob, association_rules[key][2], baskets_df.count()))
         list_assoc_rules.append([key] + association_rules[key])
-        print(key)
     
     association_rules = sc.parallelize(list_assoc_rules)
     association_rules = association_rules.filter(lambda line: line[4] > STD_LIFT_THRESHOLD)\
                                     .sortBy(lambda line: line[4])
 
-    trigrams_support_top10 = trigrams_support.sortBy(lambda line: line[1]).take(10)
-
-    print(trigrams_support_top10)
+    trigrams_support_top10 = trigrams_support.sortBy(lambda line: line[1], False).take(10)
 
     with open("{0}/Top 10 Trigrams.csv".format(sys.argv[3]), "w") as filewrite:
         filewrite.write("\n".join([t[0] for t in trigrams_support_top10]))
