71f866d997406b8329618f5aa4fbe36c5739615b
tom1k <tomascosta@ua.pt>
2021-05-20 23:00:00 +0100

    Finished CF, needs evaluation

--- Project2/Assignment2/cf.py
+++ Project2/Assignment2/cf.py
@@ -32,64 +32,53 @@ def similar_pairs(movies_ratings, num_users):
 def calculate_similarity(vector1, vector2):
     # only calculate means here, so we dont lose actual ratings on movies for next step
     mean1 = np.nanmean(vector1)
-    vector1[vector1 != np.nan] -= mean1
-    vector1 = np.nan_to_num(vector1)
+    v1 = np.nan_to_num(vector1)
+    v1[1 != np.nan] -= mean1
     
     mean2 = np.nanmean(vector2)
-    vector2[vector2 != np.nan] -= mean2
-    vector2 = np.nan_to_num(vector2)
-
+    v2 = np.nan_to_num(vector2)
+    v2[v2 != np.nan] -= mean2
     #
-    cosine_sim = np.dot(vector1, vector2) / np.sqrt(np.dot(vector1, vector1) * np.dot(vector2, vector2))
+    cosine_sim = np.dot(v1, v2) / np.sqrt(np.dot(v1, v1) * np.dot(v2, v2))
     # The similarity has no direction, so we define it in both movies
-    return cosine_sim,mean1,mean2
+    return cosine_sim
 
 def recommend_movies(users, matrix, target_movie, target_user):
     # Iteratve over every user and find k most similar pairs
     target_user = str(target_user)
     target_movie = str(target_movie)
     user_flag = -1
-    for user, ratings in users.collect():
-        if user == target_user:
-            user_flag = 1
-            most_similar = []
-            movies_watched = [movie for movie, rating in ratings]
-            for movie_id, movie_vector in matrix.items():
+    try:
+        for user, ratings in users.collect():
+            if user == target_user:
+                user_flag = 1
+                most_similar = []
+                movies_watched = [movie for movie, rating in ratings]
                 # only evaluate the movies he's seen
-                if movie_id == target_movie:
-                    for second_movie, second_vector in matrix.items():
-                        if second_movie != movie_id and second_movie in movies_watched:
-                            most_similar.append((movie_id,second_movie,calculate_similarity(movie_vector, second_vector)))
-            
-            most_similar = sorted(most_similar, key=lambda x:x[2][0], reverse=True)[:NEIGHBOUR_SIZE]
-    
-    # Calculate weighted average
-    # [('1', '500', 0.21971312123163245), ('1', '3671', 0.20316307157814495), ('1', '1270', 0.18870086378261838)]
-    if user_flag == 1:
-        print("Matrix: ", [ matrix[id2][int(target_user)-1] for id1, id2, sim in most_similar])
-        soma = 0
-        recommended_rating = float(sum([ sim*matrix[id2][int(target_user)-1] for id1, id2, sim in most_similar]) / sum([sim for id1, id2, sim in most_similar]))
-        print("Recommended rating for movie %s by user %s == %.1f" % (target_movie, target_user, recommended_rating))
-    else:
-        print("User not found, cant recommend him movies")
-
-"""
-def recommend_movies(users, matrix):
-    # Iteratve over every user and find k most similar pairs
-    user_recommendations = collections.defaultdict(lambda: [])
-    for user, ratings in users.collect():
-        most_similar = []
-        movies_watched = [movie for movie, rating in ratings]
-        for movie_id, movie_vector in matrix.items():
-            # only evaluate the movies he's seen
-            if movie_id not in movies_watched:
+                movie_vector = matrix[target_movie]
                 for second_movie, second_vector in matrix.items():
-                    if second_movie != movie_id and second_movie in movies_watched:
-                        most_similar.append((movie_id,second_movie,calculate_similarity(movie_vector, second_vector)))
-        
-        most_similar = sorted(most_similar, key=lambda x:x[2], reverse=True)[:NEIGHBOUR_SIZE]
-        print(most_similar)
-"""
+                    if second_movie != target_movie and second_movie in movies_watched:
+                        most_similar.append((target_movie,second_movie,calculate_similarity(movie_vector, second_vector)))
+                
+                most_similar = sorted(most_similar, key=lambda x:x[2], reverse=True)[:NEIGHBOUR_SIZE]
+    except:
+        print("Error, recommendation not possible")
+        sys.exit(1)
+    
+    """ # Example run for movie 1, user 1
+    [('1', '2054', 0.8144142372372292), ('1', '2018', 0.8097298933985878), ('1', '596', 0.8093382199015737)]
+    Matrix:  [4.0, 5.0, 5.0]
+    Recommended rating for movie 1 by user 1 == 4.7
+    """
+    print(most_similar)
+    if user_flag == 1:
+        rec = float(sum([ sim*matrix[id2][int(target_user)-1] for id1, id2, sim in most_similar]) / sum([sim for id1, id2, sim in most_similar]))
+        print(rec)
+        return rec
+    return 
+
+def evaluate_model(data):
+    return
 
 if __name__ == "__main__":
     sc = SparkContext(appName="CF_Recommendation")
