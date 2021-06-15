265bb09a067fa6cdf1052c052fff31e4afef7539
tom1k <tomascosta@ua.pt>
2021-06-15 19:27:13 +0100

    Having into account user intervals

--- Project3/Ex2/locations.py
+++ Project3/Ex2/locations.py
@@ -10,22 +10,25 @@ def quiet_logging(context):
 
 # Examples taken from: http://spark.apache.org/docs/latest/streaming-programming-guide.html
 if __name__ == "__main__":
-    # Create a local StreamingContext with two working thread and batch interval of 1 second
+    # Create a local StreamingContext with two working thread and batch interval of user given seconds
     sc = SparkContext(appName="LocationsStream")
-    ssc = StreamingContext(sc, 1)
+    ssc = StreamingContext(sc, int(sys.argv[2]))
     quiet_logging(sc)
 
     # Create a DStream that will connect to the data file given
-    # In this case, the default is locations.csv
-    # We need to run the program and then insert the files we want to count locations into this directory
+    # We need to run the program and only then, insert the files we want to count locations into this directory
     lines = ssc.textFileStream(sys.argv[1])
 
-    # Split each line into words
+    # Split each line into pairs (Timestamp, location)
     pairs = lines.map(lambda line: line.split("\t"))
-    pairs.pprint()
 
     counts = pairs.map(lambda location: (location[1], 1))\
-                    .reduceByKey(lambda a, b: a+b)
+                    .reduceByKey(lambda a, b: a+b)\
+
+    counts.transform(
+        lambda x: sc.parallelize(x.take(5))
+    )
+
     counts.pprint()
 
     ssc.start()             # Start the computation
